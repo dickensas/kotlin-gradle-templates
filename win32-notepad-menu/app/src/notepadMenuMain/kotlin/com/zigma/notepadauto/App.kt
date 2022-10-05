8e5ba117dfb1643a45dfd68b60cc9145ecf5e5de
das2 <das2@192.168.56.1>
2022-10-05 22:47:06 +0530

    BUFF_SIZE

--- win32-notepad-menu/app/src/notepadMenuMain/kotlin/com/zigma/notepadauto/App.kt
+++ win32-notepad-menu/app/src/notepadMenuMain/kotlin/com/zigma/notepadauto/App.kt
@@ -11,13 +11,13 @@ fun main() {
 		    var count = GetMenuItemCount(hMenu)
 		    for (i in 0 until count) {
 			    var rec = alloc<RECT>()
-			    var MATCHED_WORD_BUF_SIZE = 1024
+			    var BUFF_SIZE = 1024
 			    var fileMenu = GetMenuItemRect(hWnd, hMenu, i.toUInt(), rec.ptr)
-			    var buf = allocArray<TCHARVar>(MATCHED_WORD_BUF_SIZE)
+			    var buf = allocArray<TCHARVar>(BUFF_SIZE)
 			    var menuInfo = alloc<MENUITEMINFO>() {
 			    	cbSize = sizeOf<MENUITEMINFO>().toUInt()
 			    	fMask = MIIM_TYPE.toUInt() or MIIM_SUBMENU.toUInt()
-			    	cch = MATCHED_WORD_BUF_SIZE.toUInt()
+			    	cch = BUFF_SIZE.toUInt()
 			    	dwTypeData = buf
 			    }
 			    GetMenuItemInfoA(hMenu,i.toUInt(),1,menuInfo.ptr)
