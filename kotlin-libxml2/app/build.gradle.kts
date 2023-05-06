000ebdcf032944af60586feb8614f211709b6adf
dickensas <dickens.as@gmail.com>
2023-05-07 01:03:13 +0530

    libxml2

--- kotlin-libxml2/app/build.gradle.kts
+++ kotlin-libxml2/app/build.gradle.kts
@@ -32,6 +32,13 @@ kotlin {
             }
         }
         compilations["main"].cinterops {
+            val libxml2 by creating {
+               when (preset) {
+                  presets["mingwX64"] -> includeDirs("C:/msys64/mingw64/include", "C:/msys64/mingw64/include/libxml2", "C:/msys64/mingw64/include/libxslt")
+				  presets["linuxX64"] -> includeDirs("${project.rootDir}/include", "/usr/include", "/usr/include/libxml2", "/usr/include/libxslt")
+				  presets["macosX64"] -> includeDirs("${project.rootDir}/include", "/usr/include", "/usr/include/libxml2", "/usr/include/libxslt")
+               }
+            }
             val libxslt by creating {
                when (preset) {
                   presets["mingwX64"] -> includeDirs("C:/msys64/mingw64/include", "C:/msys64/mingw64/include/libxml2", "C:/msys64/mingw64/include/libxslt")
