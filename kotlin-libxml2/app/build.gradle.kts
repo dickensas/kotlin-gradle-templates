cfd8b4a143a1da597c86ecabb5c493867f37efe6
dickensas <dickens.as@gmail.com>
2023-05-06 22:50:03 +0530

    os independent gradle

--- kotlin-libxml2/app/build.gradle.kts
+++ kotlin-libxml2/app/build.gradle.kts
@@ -1,5 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+import java.io.*
 plugins {
     id("org.jetbrains.kotlin.multiplatform") version "1.8.10"
 }
@@ -13,29 +13,34 @@ repositories {
 }
 
 kotlin {
-    mingwX64("libgnuplot") {
+   val hostOs = System.getProperty("os.name")
+    if (hostOs == "Mac OS X") {
+        macosX64("libgnuplot")
+    }
+    if (hostOs == "Linux") {
+        linuxX64("libgnuplot")
+    }
+    if (hostOs.startsWith("Windows")) {
+        mingwX64("libgnuplot")
+    }
+    
+    targets.withType<KotlinNativeTarget> {
         binaries {
             executable {
                 entryPoint = "plot.main"
-                linkerOpts("-L${userHome}\\.konan\\dependencies\\msys2-mingw-w64-x86_64-2\\x86_64-w64-mingw32\\lib", "-L${project.rootDir}")
+                when (preset) {
+					presets["mingwX64"] -> linkerOpts("-L${userHome}\\.konan\\dependencies\\msys2-mingw-w64-x86_64-2\\x86_64-w64-mingw32\\lib", "-L${project.rootDir}")
+					presets["linuxX64"] -> linkerOpts("-L${project.rootDir}")
+					presets["macosX64"] -> linkerOpts("-L${project.rootDir}")
+				}
             }
         }
         compilations["main"].cinterops {
-            val libxml2 by creating {
-               includeDirs {
-                  allHeaders(
-					 "C:\\msys64\\mingw64\\include",
-                     "C:\\msys64\\mingw64\\include\\libxml2"
-                  )
-               }
-            }
-            
             val libxslt by creating {
-               includeDirs {
-                  allHeaders(
-					 "C:\\msys64\\mingw64\\include",
-					 "C:\\msys64\\mingw64\\include\\libxml2"
-                  )
+               when (preset) {
+                  presets["mingwX64"] -> includeDirs("C:/msys64/mingw64/include", "C:/msys64/mingw64/include/libxml2")
+				  presets["linuxX64"] -> includeDirs("/usr/include", "/usr/include/libxml2")
+				  presets["macosX64"] -> includeDirs("/usr/include", "/usr/include/libxml2")
                }
             }
         }
