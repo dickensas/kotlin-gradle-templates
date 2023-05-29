7da90e14d2bcc44d6e50e095efdfd5e74fe063bf
dickensas <dickens.as@gmail.com>
2023-05-29 20:56:44 +0530

    triangle app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,9 @@ pluginManagement {
     }
 }
 rootProject.name="kotlin-gradle-templates"
+include("kotlin-libxml2:app")
 include("kotlin-cpp:cpplib")
 include("kotlin-cpp:cpplib_c")
 include("kotlin-cpp")
 include("sboot-mockito-integration")
+include("vulkan-triangle-glfw")
