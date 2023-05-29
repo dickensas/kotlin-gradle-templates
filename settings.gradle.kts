52d59a10e523e6c4afd4c03343476f9e507476eb
dickensas <dickens.as@gmail.com>
2023-05-29 22:10:28 +0530

    vulkan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,4 @@ pluginManagement {
     }
 }
 rootProject.name="kotlin-gradle-templates"
-include("kotlin-libxml2:app")
-include("kotlin-cpp:cpplib")
-include("kotlin-cpp:cpplib_c")
-include("kotlin-cpp")
 include("vulkan-triangle-glfw")
