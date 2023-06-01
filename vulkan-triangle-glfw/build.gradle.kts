3974d1a791fa92fb984b967556f19c80fb48791d
dickensas <dickens.as@gmail.com>
2023-06-01 18:18:47 +0530

    chmod

--- vulkan-triangle-glfw/build.gradle.kts
+++ vulkan-triangle-glfw/build.gradle.kts
@@ -28,7 +28,7 @@ kotlin {
                 entryPoint = "plot.main"
                 when (preset) {
 					presets["mingwX64"] -> linkerOpts("-L${userHome}\\.konan\\dependencies\\msys2-mingw-w64-x86_64-2\\x86_64-w64-mingw32\\lib", "-L${project.rootDir}", "-lm", "-lc", "-ldl")
-					presets["linuxX64"] -> linkerOpts("-L${userHome}/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/x86_64-unknown-linux-gnu/lib64","-L${userHome}/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/sysroot/usr/lib64","-L${project.rootDir}", "-lm", "-lc", "-ldl")
+					presets["linuxX64"] -> linkerOpts("-L${userHome}/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/x86_64-unknown-linux-gnu/lib64","-L${userHome}/.konan/dependencies/x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2/x86_64-unknown-linux-gnu/sysroot/usr/lib","-L${project.rootDir}", "-lm", "-lc", "-ldl")
 					presets["macosX64"] -> linkerOpts("-L${project.rootDir}")
 				}
             }
