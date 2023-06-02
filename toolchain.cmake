81b9d46f700e679b6c31e1d370d40aa3ea68e605
dickensas <dickens.as@gmail.com>
2023-06-02 12:53:36 +0530

    -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake

---
+++ toolchain.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_FLAGS "-L/lib -L/lib/x86_64-linux-gnu -L/usr/lib -L/usr/lib/x86_64-linux-gnu -L/usr/lib32 -L/usr/lib64 -L/usr/x86_64-linux-gnu/lib -L/usr/x86_64-linux-gnu/lib32 -ldl -lm -lstdc++ -lc")
+set(CMAKE_CXX_FLAGS "-L/lib -L/lib/x86_64-linux-gnu -L/usr/lib -L/usr/lib/x86_64-linux-gnu -L/usr/lib32 -L/usr/lib64 -L/usr/x86_64-linux-gnu/lib -L/usr/x86_64-linux-gnu/lib32 -ldl -lm -lstdc++ -lc")
